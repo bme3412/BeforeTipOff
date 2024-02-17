3cb72c05930a36e22a771470c57473dd637bd206
bme3412 <54747855+bme3412@users.noreply.github.com>
2024-02-17 18:12:30 -0500

    Delete sportsInfo directory

--- sportsInfo/README.md
+++
@@ -1,6 +0,0 @@
-BeforeTheGame
-
-
-Full stack app that provides local info about sports events
-
-What I want is to be able to get any info
--- sportsInfo/static/js/script.js
+++
@@ -1,9 +0,0 @@
-<script src="{{ url_for('static', filename='js/script.js') }}"></script>
-document.addEventListener('DOMContentLoaded', function() {
-    var searchButton = document.getElementById('search-btn');
-    searchButton.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent the default form submission
-        var form = document.querySelector('form');
-        form.submit(); // Programmatically submit the form
-    });
-});
